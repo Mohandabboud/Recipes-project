59ddb008b0ae41143e4fe356a31eb4906122c800
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-25 12:33:00 +0100

    show result function

--- SeaFun.js
+++ SeaFun.js
@@ -1,6 +1,6 @@
 function ChangeHref() {
 var x = document.querySelector("#input").value;
-var url = new URL("http://127.0.0.1:8000/searchResult.html")
+var url = new URL("http://127.0.0.1:8000/searchResult2.html")
 url.searchParams.append("search", x)
 var b = document.querySelector(".btn")
 b.href = url;
---
+++ searchResult2.js
@@ -0,0 +1,163 @@
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get("search");
+    fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=13036d4464844ed8bf5e1bb603bb0a6b&includeIngredients="+myParam)
+    .then(response => response.json())
+    .then( data => {
+        console.log(data)
+        data.results.forEach( function (item) {
+            fetch(" https://api.spoonacular.com/recipes/"+ item.id +"/information?apiKey=13036d4464844ed8bf5e1bb603bb0a6b")
+            .then(response => response.json())
+            .then( recipe => { console.log(recipe)
+                var card = document.createElement("div");
+                card.className = "card";
+                var container = document.createElement("div");
+                container.className = "container";
+                var image = document.createElement("img")
+                image.src = recipe.image;
+                image.className = "image11";
+                var h = document.createElement("h1");
+                h.innerHTML = recipe.title;
+                var p = document.createElement("p");
+                p.innerHTML = recipe.summary;
+                var a = document.createElement("a");
+                a.href = "http://127.0.0.1:8000/descript.html";
+                a.className = "link11"
+                var button = document.createElement("button");
+                button.innerHTML = "click here";
+                var article = document.createElement("article");
+                article.className = "article";
+                var table = document.getElementsByClassName("table")[0];
+                table.appendChild(card);
+                card.appendChild(image);
+                //card.appendChild(container);
+                article.appendChild(h);
+                article.appendChild(p);
+                card.appendChild(a);
+                //container.appendChild(button);
+                a.appendChild(article);
+             //var p = document.querySelector("#dd");
+               //     p.innerHTML = recipe.title 
+               /*
+                               console.log(recipe.title)
+                               console.log(recipe.aggregateLikes)
+                               console.log(recipe.readyInMinutes)
+                               console.log(recipe.servings)
+                               //console.log(recipe.instructions)
+                               console.log(recipe.summary)
+                               //console.log(recipe.analyzedInstructions[0].steps)
+                               recipe.analyzedInstructions[0].steps.forEach( function (item) {
+                                   console.log(item.step)
+                                          
+                                })
+                               //console.log(recipe.extendedIngredients) 
+                               recipe.extendedIngredients.forEach( function (item) {
+                                    console.log(item.originalName)
+                                      
+                               })
+                               recipe.extendedIngredients.forEach( function (item) {
+                               console.log(item.originalString)
+                               }) */
+                               
+            })
+        })
+    })
+
+   // var a = document.querySelector(".btn")
+   // a.addEventListener("click",searchBar);
+    
+    
+    /*
+    function searchBar() {
+        fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=9b19014764264e228cd8cfd7de2b2457&ingredients=apples,+flour,+sugar&number=2")
+        .then(response => response.json())
+        .then( data => console.log(data))
+    }
+    searchBar();
+    */
+    
+    /* fetch("https://api.spoonacular.com/recipes/716381/analyzedInstructions?apiKey=9b19014764264e228cd8cfd7de2b2457")
+            .then(response => response.json())
+            .then( recipe => { console.log(recipe)
+             forEach.recipe[0]( function (item) {
+                 console.log(recipe.steps)
+             })
+                })        
+            */
+    
+      /*         
+    fetch(" https://api.spoonacular.com/recipes/716381/information?apiKey=9b19014764264e228cd8cfd7de2b2457")
+    .then(response => response.json())
+    .then( recipe => {console.log(recipe)
+                       console.log(recipe.aggregateLikes)
+                       console.log(recipe.readyInMinutes)
+                       console.log(recipe.servings)
+                       console.log(recipe.instructions)
+                       console.log(recipe.summary)
+                       console.log(recipe.analyzedInstructions[0].steps)
+                       recipe.analyzedInstructions[0].steps.forEach( function (item) {
+                           console.log(item.step)
+                                  
+                        })
+                       console.log(recipe.extendedIngredients) 
+                       recipe.extendedIngredients.forEach( function (item) {
+                            console.log(item.originalName)
+                              
+                       })
+                       recipe.extendedIngredients.forEach( function (item) {
+                       console.log(item.originalString)
+                       })
+                       
+    }) */
+/*
+    function searchBar() {
+        var x = document.querySelector("#input").value;
+        if (x) {
+        var p = document.createElement("p");
+        var body = document.querySelector("body")
+        p.innerHTML = x;
+        console.log(p);
+        body.appendChild(p)
+        fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=9b19014764264e228cd8cfd7de2b2457&includeIngredients="+x)
+        .then(response => response.json())
+        .then( data => {
+            console.log(data)
+            var div = document.createElement("div")
+            data.results.forEach( function (item) {
+                var p1 = document.createElement("p")
+                p1.innerHTML = item.title;
+                body.appendChild(p1)
+                console.log(item.title)
+                console.log(item.id)
+                var image = document.createElement("img");
+                image.src = item.image;
+                body.appendChild(image);
+                fetch(" https://api.spoonacular.com/recipes/"+ item.id +"/information?apiKey=9b19014764264e228cd8cfd7de2b2457")
+                .then(response => response.json())
+                .then( recipe => { console.log(recipe)
+                                   console.log(recipe.title)
+                                   console.log(recipe.aggregateLikes)
+                                   console.log(recipe.readyInMinutes)
+                                   console.log(recipe.servings)
+                                   //console.log(recipe.instructions)
+                                   console.log(recipe.summary)
+                                   //console.log(recipe.analyzedInstructions[0].steps)
+                                   recipe.analyzedInstructions[0].steps.forEach( function (item) {
+                                       console.log(item.step)
+                                              
+                                    })
+                                   //console.log(recipe.extendedIngredients) 
+                                   recipe.extendedIngredients.forEach( function (item) {
+                                        console.log(item.originalName)
+                                          
+                                   })
+                                   recipe.extendedIngredients.forEach( function (item) {
+                                   console.log(item.originalString)
+                                   })
+                                   
+                })
+            })
+        })}
+        }
+        var a = document.querySelector(".btn")
+        a.addEventListener("click",searchBar);
+        */
