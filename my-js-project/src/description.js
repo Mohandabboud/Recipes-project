a90aa7d45a9470387cc00980cd2d6e7e3b072d83
Siar Sohrab <siar.sohrab@gmail.com>
2021-04-02 15:41:20 +0200

    final discrption page and result page

--- my-js-project/src/description.js
+++ my-js-project/src/description.js
@@ -1,11 +1,14 @@
-//var firstAPI = "8a07d27c762a4515b4eb63220b517e02";
+var firstAPI = "8a07d27c762a4515b4eb63220b517e02";
 var secondAPI = "240f675c9ad44d34a96f35d6cb97542d";
-//var thirdAPI = "1c3d08b06c8e4d0189ef89e62b169eba";
+var thirdAPI = "1c3d08b06c8e4d0189ef89e62b169eba";
 var fourthAPI = "caaf06b217bb42b8987e7cafc76673eb";
 var fifthAPI = "b2f27322a1f347379d89b490ed9528e9";
 var sixthAPI = "81d2fb7ce8bb447e8d0e372ccf4d79d0";
 var seventhAPI = "5dc571ad8c4442e3be0ebea89c223437";
 var eighthAPI = "144b024c11254237bd7586886f98dfec";
+var ninthApi = "159bac7d7f9f413d931d0a6ee0a32b2e";
+var tenthApi = "ee9ef8255c22455e9a3d2678137330d2";
+var eleventhApi = "780aeeca39d34a1bab3789be644f5daf";
 
 
 
@@ -13,7 +16,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get("search");
 var instruction = [];
 var ingredient = [];
-fetch(" https://api.spoonacular.com/recipes/"+ myParam +"/information?apiKey="+ eighthAPI)
+fetch(" https://api.spoonacular.com/recipes/"+ myParam +"/information?apiKey="+ firstAPI)
 .then(response => response.json())
 .then( recipe => {
     console.log(recipe)
